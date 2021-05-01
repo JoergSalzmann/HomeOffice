c93c952564dbed459a9d3397549b407bcaf3de54
Jörg@HomePC <joerg_salzmann@gmx.de>
2021-05-01 15:35:31 +0200

    Pakete aktualisiert: Ribbon.Fork.NET5 -> 5.0.4 / WakeOnLanCSM -> 2.2

--- HomeOffice/MainView.cs
+++ HomeOffice/MainView.cs
@@ -42,8 +42,8 @@ namespace HomeOffice
                 stringBuilder.AppendLine(Text);
                 stringBuilder.AppendLine();
                 stringBuilder.AppendLine("Verwendete Pakete:");
-                stringBuilder.AppendLine("- Ribbon.Fork.NET5  5.0-RC1");
-                stringBuilder.AppendLine("- WakeOnLanCSM  2.2-RC2");
+                stringBuilder.AppendLine("- Ribbon.Fork.NET5  5.0.4");
+                stringBuilder.AppendLine("- WakeOnLanCSM  2.2");
                 stringBuilder.AppendLine();
                 stringBuilder.AppendLine("© 2020-2021 - J. Salzmann");
                 MessageBox.Show(stringBuilder.ToString(), "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
